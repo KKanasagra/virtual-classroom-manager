7e9aee0afda4a5e86437b6bc95a611ac14565ceb
KKanasagra <129594755+KKanasagra@users.noreply.github.com>
2023-10-06 10:23:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,150 @@
+import logging                      # For Providing logging funcitionality to the code
+import pyfiglet  
+
+# For ascii art welcome message
+def custom_welcome_message():
+    # ASCII art using pyfiglet
+    ascii_art = pyfiglet.figlet_format("Welcome to Virtual Classroom Manager Program", font="slant")
+    print(ascii_art)
+
+
+
+# Creating a Student class
+class Student:
+    def __init__(self, student_ID, student_class):
+        self.student_ID = student_ID
+        self.student_class = student_class
+
+
+# Creating a ClassRoom class
+class ClassRoom:
+    def __init__(self, class_name):
+        self.class_name = class_name
+        self.class_students = {}  
+        self.class_assignments = []
+
+    # Method for enrolling students into classroom
+    def enroll(self, student):
+        student_ID = student.student_ID
+        if student_ID not in self.class_students:
+            self.class_students[student_ID] = student
+        else:
+            print(f"Student {student_ID} is already enrolled in '{self.class_name}'.")
+
+    # Method for listing students in class
+    def list_students(self):
+        print(f"Students in ClassRoom '{self.class_name}'")
+        for student_ID, student in self.class_students.items():
+            print(f"Student : {student_ID}")  
+
+    # Method for assignment scheduling 
+    def schedule_assignment(self, assignment_description):
+        self.class_assignments.append(assignment_description)
+
+
+    def __str__(self):
+        return f"ClassRoom: {self.class_name}"
+    
+
+# Creating Assignment class
+class Assignment:
+    def __init__(self, assignment_description):
+        self.assignment_description = assignment_description
+        self.is_submitted = False
+
+
+# ----------------------------------------------------------------------------------------------        
+
+
+# Creating VirtualClassRoomManager class for managing Virtual classroom related activites.
+class VirtualClassRoomManager:
+    def __init__(self):
+        self.classes = []
+        
+        # the code enables logging for our program and helps in debugging
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', '%H:%M:%S')
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+    # Method for classroom creation
+    def create_Room(self, class_name):
+        if not any(cls.class_name == class_name for cls in self.classes):
+            classroom = ClassRoom(class_name)
+            self.classes.append(classroom)
+            self.logger.info(f"Classroom '{class_name}' has been created.")
+        else:
+            self.logger.warning(f"Classroom '{class_name}' already exists.")
+
+    # Method for classroom removal
+    def remove_Room(self, class_name):
+        for cls in self.classes:
+            if cls.class_name == class_name:
+                self.classes.remove(cls)
+                self.logger.info(f"Classroom '{class_name}' has been removed.")
+                return
+        self.logger.warning(f"Classroom '{class_name}' not found.")
+
+    # Method for listing classroom
+    def list_Room(self):
+        for classs in self.classes:
+            print(classs)
+
+    # Method for finding classroom
+    def find_Room(self, class_name):
+        for classs in self.classes:
+            if classs.class_name == class_name:
+                return classs
+        return None
+
+
+    # Method for Adding/Enrolling students into classroom
+    def enroll_student(self, student, class_name):
+        classroom = self.find_Room(class_name)
+        if classroom:
+            student_ID = student.student_ID
+
+            # Check if the student is already enrolled in the class
+            if student_ID in classroom.class_students:
+                self.logger.warning(f"Student {student_ID} is already enrolled in '{class_name}'.")
+            else:
+                classroom.enroll(student)
+                self.logger.info(f"Student {student_ID} has been enrolled in '{class_name}'.")
+
+        else:
+            self.logger.warning(f"Classroom '{class_name}' not found.")
+
+    # Method for listing students in classroom
+    def student_in_classroom(self, class_name):
+        classroom = self.find_Room(class_name)
+
+        if classroom:
+            classroom.list_students()
+        else:
+            self.logger.warning(f"Classroom '{class_name}' not found.")
+
+    # Method for scheduling assignment
+    def schedule_assignment(self, class_name, assignment_description):
+        classroom = self.find_Room(class_name)
+        if classroom:
+            classroom.schedule_assignment(assignment_description)
+            self.logger.info(f"Assignment for '{class_name}' has been scheduled.")
+        else:
+            self.logger.warning(f"Classroom '{class_name}' not found.")
+
+    # Method for assignment submission
+    def submit_assignment(self, student_ID, class_name, assignment_description):
+        classroom = self.find_Room(class_name)
+        if classroom:
+            for student in classroom.class_students:
+                if student.student_ID == student_ID:
+                    assignment = Assignment(assignment_description)
+                    assignment.is_submitted = True
+                    self.logger.info(f"Assignment submitted by Student {student_ID} in '{class_name}'.")
+                    return
+            self.logger.warning(f"Student ID '{student_ID}' not found in '{class_name}'.")
+        else:
+            self.logger.warning(f"Classroom '{class_name}' not found.")
+
---
+++ runner.py
@@ -0,0 +1,91 @@
+from last import *
+
+if __name__ == "__main__":
+    custom_welcome_message()
+    classroom_manager = VirtualClassRoomManager()
+
+    while True:
+        print("\nCommands:")
+        print("1. add_classroom [class_name]")
+        print("2. add_student [student_ID] [class_name]")
+        print("3. schedule_assignment [class_name] [assignment_details]")
+        print("4. submit_assignment [student_ID] [class_name] [assignment_details]")
+
+        print("\nAdditional Functionalities") 
+
+        print("5. remove_classroom [class_name]")
+        print("6. list_classroom ")
+        print("7. students_in_class [class_name]")
+        print("8. exit \n")
+
+
+        user_input = input("Enter a command: ").strip().split()
+
+
+        if user_input[0] == "add_classroom":
+            if len(user_input) != 2:
+                print("Usage: add_classroom [class_name]")
+            else:
+                class_name = user_input[1]
+                classroom_manager.create_Room(class_name)
+
+
+        elif user_input[0] == "add_student":
+            if len(user_input) != 3:
+                print("Usage: add_student [student_ID] [class_name]")
+            else:
+                student_ID = user_input[1]
+                class_name = user_input[2]
+                student = Student(student_ID, class_name)
+                classroom_manager.enroll_student(student, class_name)
+
+
+        elif user_input[0] == "schedule_assignment":
+            if len(user_input) < 3:
+                print("Usage: schedule_assignment [class_name] [assignment_details]")
+            else:
+                class_name = user_input[1]
+                assignment_details = " ".join(user_input[2:])
+                classroom_manager.schedule_assignment(class_name, assignment_details)
+
+
+        elif user_input[0] == "submit_assignment":
+            if len(user_input) < 4:
+                print("Usage: submit_assignment [student_ID] [class_name] [assignment_details]")
+            else:
+                student_ID = user_input[1]
+                class_name = user_input[2]
+                assignment_details = " ".join(user_input[3:])
+                classroom_manager.submit_assignment(student_ID, class_name, assignment_details)
+
+        # For Additional Functionalities
+
+        elif user_input[0] == "remove_classroom":
+            if len(user_input) != 2:
+                print("Usage: remove_classroom [class_name]")
+            else:
+                class_name = user_input[1]
+                classroom_manager.remove_Room(class_name) 
+
+        elif user_input[0] == "list_classroom":
+            if len(user_input) != 1:
+                print("Usage: list_classroom")
+            else:
+                classroom_manager.list_Room()
+
+        elif user_input[0] == "students_in_class":
+            if len(user_input) != 2:
+                print("Usage: students_in_class [class_name]")
+            else:
+                class_name = user_input[1]
+                classroom_manager.student_in_classroom(class_name)
+
+        elif user_input[0] == "exit":
+            print("Exiting the Virtual Classroom Manager.")
+            break
+
+        else:
+            print("Invalid command. Please try again.")
+
+
+    
